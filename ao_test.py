8e4bf51f7d9266eb1ced9dc6c77cdcf18e39ada2
artwebs <arwebs@126.com>
2018-03-19 00:41:28 +0800

    代码整理

--- ao_test.py
+++ ao_test.py
@@ -2,8 +2,8 @@
 from ao import *
 
 def test_db():
-      _db = db_init('mysql', connstr="connstr")
-      _db.conn()
+    _db = db_init('mysql', connstr="connstr")
+    _db.conn()
 
 if __name__ == '__main__':
-      test_db()
+    test_db()
